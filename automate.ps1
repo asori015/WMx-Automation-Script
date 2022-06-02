5b36e79637c7b1af90f56eeddc637e31eb8e711f
Soria Limon <asorialimon@newbreed.com>
2022-06-02 06:54:20 -0700

    Addded more notes. Also added powershell run script for windows.

---
+++ automate.ps1
@@ -0,0 +1,12 @@
+# cd WMX-active 
+# ../python-3.9.10-embed-amd64/python.exe automate.py -f -t 16 -l 0000029453 -u tponce -p Welcome1234567  #
+# cd ..
+
+#../python-3.9.10-embed-amd64/python.exe automate.py -f -t 16 -u asorialimon -p Welcome12345 -e totes.xlsx -l 0000031012
+
+# IGNORE ABOVE
+############################################
+# YOU SHOULD ONLY FOCUS ON THIS
+
+python automate.py -f -t 0 -u asorialimon -p Welcome12345 -e totes.xlsx # -l 0000031012
+# python -h
